6cfb5e4365548ef22e48acc405b6c18f1a58720c
Zoe Abrams <zoej.abrams@gmail.com>
2020-11-01 14:30:57 -0800

    Style launch screen

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by user179480 on 11/1/20.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
